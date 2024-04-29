9f57404657fa542b2f353a83995cf9769c658abe
lenoel777 <glenn.leonali@binus.ac.id>
2024-04-29 22:12:23 +0700

    photo button di ContentView done

--- WandARaw/View/ContentView.swift
+++ WandARaw/View/ContentView.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let viewController = ARManager.shared
+    @State private var isPhotoTapped = false
+    
     var body: some View {
         ZStack(content: {
             ARViewContainer()
                 .edgesIgnoringSafeArea(.all)
-                // Add button here
+            
+            // Crosshair
+            Text("+")
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.green)
+            
+            // Button
+            VStack(alignment: .trailing) {
+                Spacer()
+                Button(action: {
+                    takePhoto()
+                }, label: {
+                    Image(systemName: "camera.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50)
+                        .padding()
+                        .foregroundStyle(Color.black)
+                })
+            }
+            
+            // Photo Taken Indicator
+            Rectangle()
+                .fill(.white)
+                .ignoresSafeArea()
+                .opacity(isPhotoTapped ? 1.0 : 0.0)
+                .animation(.easeInOut(duration: 0.3), value: isPhotoTapped)
         })
     }
+    
+    func takePhoto() {
+        isPhotoTapped = true
+        UIImageWriteToSavedPhotosAlbum(viewController.sceneView.snapshot(), viewController, nil, nil)
+        print("\(isPhotoTapped)")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            isPhotoTapped = false
+        }
+    }
+    
 }
 
 #Preview {
