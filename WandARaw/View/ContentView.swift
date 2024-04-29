f3ec072b723476ebe4b3d8281569fbda7055ca97
Risa <indriklarissa@gmail.com>
2024-04-29 19:32:14 +0700

    UI components only

--- WandARaw/View/ContentView.swift
+++ WandARaw/View/ContentView.swift
@@ -13,6 +13,37 @@ struct ContentView: View {
             ARViewContainer()
                 .edgesIgnoringSafeArea(.all)
                 // Add button here
+            
+            VStack {
+                HStack {
+//                  Trash Icon
+                    IconButton(imageName: "trash", iconSize: 25, buttonFill: false, label: "trash") {
+                        print("Trash tapped") // Handle button tap action
+                    }
+                    Spacer()
+//                  Info Icon
+                    IconButton(imageName: "trash", iconSize: 25, buttonFill: false, label: "trash2") {
+                        print("Info tapped") // Handle button tap action
+                    }
+                }
+                Spacer()
+                HStack(alignment: .center, spacing: 40) {
+//                  Color Icon
+                    IconButton(imageName: "camera", iconSize: 35, buttonFill: false, label: "color") {
+                        print("Color tapped") // Handle button tap action
+                    }
+//                  Brush Icon
+                    IconButton(imageName: "brush", iconSize: 35, buttonFill: true, label: "brush") {
+                        print("Brush tapped") // Handle button tap action
+                    }
+                    
+//                  Camera Icon
+                    IconButton(imageName: "camera", iconSize: 35, buttonFill: false, label: "camera") {
+                        print("Camera tapped") // Handle button tap action
+                    }
+                }
+                .padding(16)
+            }
         })
     }
 }
