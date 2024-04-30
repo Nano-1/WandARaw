47f9343a1720054da1168670a42f9bf6a98b5fe1
Sherly Phangestu <phangestusherly@gmail.com>
2024-04-30 09:22:40 +0700

    Merge remote-tracking branch 'refs/remotes/origin/uiComponents'
    Conflicts:
    	WandARaw/View/ContentView.swift

---
+++ WandARaw/View/IconButton.swift
@@ -0,0 +1,46 @@
+//
+//  IconButton.swift
+//  WandARaw
+//
+//  Created by Risa on 29/04/24.
+//
+
+import SwiftUI
+
+struct IconButton: View {
+    let imageName: String
+    let iconSize: CGFloat
+    let buttonFill: Bool
+    let label: String
+    var action: () -> Void
+    
+    @State private var isPressed = false
+
+    var body: some View {
+        Button(action: {
+            self.action()
+        }) {
+        Circle()
+            .fill(buttonFill == true ? .white : .clear)
+            .overlay(Image(imageName)
+                .resizable()
+                .frame(width: iconSize, height: iconSize)
+            )
+        .frame(width: 72, height: 72)
+        .scaleEffect(isPressed ? 1.28 : 1.0)
+        }
+        .simultaneousGesture(
+            DragGesture(minimumDistance: 0)
+                .onChanged { _ in
+                    withAnimation {
+                        self.isPressed = true
+                    }
+                }
+                .onEnded { _ in
+                    withAnimation {
+                        self.isPressed = false
+                    }
+                }
+        )
+    }
+}
